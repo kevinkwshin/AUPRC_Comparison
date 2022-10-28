da688f8991777db1a839e266e454b6378317fa83
Keewon Shin <kevinkwshin@gmail.com>
2022-10-28 15:13:10 +0900

    Update auprc_comparison.py

--- auprc_comparison.py
+++ auprc_comparison.py
@@ -6,32 +6,40 @@ def pValue(mean_ref,std_ref,mean_target, std_target,df):
     return scipy.stats.t.sf(abs(t), df=df-1)*2
 
 def AUPRC_comparison(y_ref, yhat_ref, yhat_target, bootstrap=10):
+    """
+    y_ref : ground truth consisted with 0, 1
+    yhat_ref : likelihood value of reference model consisted with 0 ~ 1
+    yhat_target : likelihood value of target model consisted with 0 ~ 1
+
+    example:
+    a = np.array([0,0,0,0,0,1,1,1,1,1])
+    b = np.array([0.3,0.2,0.4,0.5,0.6,.7,.6,.8,.3,.4])
+    c = np.array([0.1,0.1,0.1,0.5,0.4,.7,.6,.8,.3,.4])
+
+    AUPRC_comparison(a,b,c)
     
-    assert len(y_ref)==len(yhat_ref) == len(yhat_target)
-    yhat_ref_means =[]
-    yhat_target_means =[]
+    """
+    assert len(y_ref) == len(yhat_ref) == len(yhat_target)
+
+    auprcs_ref = []
+    auprcs_target = []
     
     for i in range(bootstrap):
-        sample = sklearn.model_selection.train_test_split(y_ref, yhat_ref, yhat_target, train_size=0.8, random_state=None)
-        y_ref_sample, _, yhat_ref_sample, _, yhat_target_sample,_ = sample
+        # sampling
+        y_ref_sample, _, yhat_ref_sample, _, yhat_target_sample,_ = sklearn.model_selection.train_test_split(y_ref, yhat_ref, yhat_target, train_size=0.8, random_state=None)
         
+        # compute AUPRC
         auprc_ref = sklearn.metrics.average_precision_score(y_ref_sample,yhat_ref_sample)
         auprc_target = sklearn.metrics.average_precision_score(y_ref_sample,yhat_target_sample)
         
-        yhat_ref_means.append(auprc_ref)
-        yhat_target_means.append(auprc_target)
+        auprcs_ref.append(auprc_ref)
+        auprcs_target.append(auprc_target)
         
-    mean_ref = np.mean(yhat_ref_means)
-    std_ref = np.std(yhat_ref_means)
+    mean_ref = np.mean(auprcs_ref)
+    std_ref = np.std(auprcs_ref)
     
-    mean_target = np.mean(yhat_target_means)
-    std_target = np.std(yhat_target_means)
+    mean_target = np.mean(auprcs_target)
+    std_target = np.std(auprcs_target)
     pvalue = pValue(mean_ref, std_ref, mean_target, std_target, bootstrap)
      
     return {'auprc_ref':auprc_ref, 'auprc_target':auprc_target, 'pvalue':pvalue}
-
-a = np.array([0,0,0,0,0,1,1,1,1,1])
-b = np.array([0.3,0.2,0.4,0.5,0.6,.7,.6,.8,.3,.4])
-c = np.array([0.1,0.1,0.1,0.5,0.4,.7,.6,.8,.3,.4])
-
-AUPRC_comparison(a,b,c)
